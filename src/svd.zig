9e218804a6ebb2a244e7a69086bb020cfd4198cf
vegecode <justin.b.alexander1@gmail.com>
2020-02-09 16:51:27 -0600

    Add printing of cpu

--- src/svd.zig
+++ src/svd.zig
@@ -93,6 +93,35 @@ pub const Cpu = struct {
         self.revision.deinit();
         self.endian.deinit();
     }
+
+    pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, context: var, comptime Errors: type, output: fn (@TypeOf(context), []const u8) Errors!void) Errors!void {
+        try output(context, "\n");
+
+        const name = if (self.name.len() == 0) "unknown" else self.revision.toSliceConst();
+        const revision = if (self.revision.len() == 0) "unknown" else self.revision.toSliceConst();
+        const endian = if (self.endian.len() == 0) "unknown" else self.endian.toSliceConst();
+        const mpu_present = self.mpu_present orelse false;
+        const fpu_present = self.mpu_present orelse false;
+        const vendor_systick_config = self.vendor_systick_config orelse false;
+        try std.fmt.format(context, Errors, output,
+            \\pub const cpu = struct {{
+            \\    pub const name = {};
+            \\    pub const revision = {};
+            \\    pub const endian = {};
+            \\    pub const mpu_present = {};
+            \\    pub const fpu_present = {};
+            \\    pub const vendor_systick_config = {};
+            \\
+        , .{ name, revision, endian, mpu_present, fpu_present, vendor_systick_config });
+        if (self.nvic_prio_bits) |prio_bits| {
+            try std.fmt.format(context, Errors, output,
+                \\    pub const nvic_prio_bits = {};
+                \\
+            , .{prio_bits});
+        }
+        try output(context, "};");
+        return;
+    }
 };
 
 pub const Peripherals = ArrayList(Peripheral);
